3bab3d302489dcf180db156efeb0e044ca047f9a
Theo Sementa <theosementa@gmail.com>
2025-12-09 13:13:02 +0100

    feat: init project

---
+++ Dreevy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dreevy
+//
+//  Created by Theo Sementa on 09/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dreevy/DreevyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreevyApp.swift
+//  Dreevy
+//
+//  Created by Theo Sementa on 09/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct DreevyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
